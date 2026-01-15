87ef9e49aa50a78afb27df8d64f72e6f7ff76f5b
M K V N S MANIKANTA <24pa1a4573@vishnu.edu.in>
2026-01-15 11:19:42 +0530

    Add files via upload

---
+++ ProgramName.java
@@ -0,0 +1,10 @@
+/*
+ * Program Name: Hello World.
+ * Description: This program prints a greeting message.
+ */
+
+public class ProgramName {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
